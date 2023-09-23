2934a2cd8fd6a9ee960f8348095b601bc83437cd
PremPremPremPrem <prem.sy1952@gmail.com>
2023-09-23 21:58:37 +0700

    editor
    e

--- stuffs/editor-scripts.js
+++ stuffs/editor-scripts.js
@@ -4,6 +4,15 @@ const edit_name_div = document.getElementById('edit-name-dialog-container');
 const edit_name_button_real = document.getElementById('edit-name-button');
 const project_name = document.getElementById('project-name');
 
+const watchID = navigator.geolocation.watchPosition((position) => {
+    updatePosition(position.coords.latitude, position.coords.longitude);
+});
+
+var lat, lng;
+function updatePosition(lati, lngi) {
+    [lat, lng] = [lati, lngi];
+}
+
 function shake_anim(el) {
     el.addEventListener('mouseover', () => {el.classList.add('fa-shake')});
     el.addEventListener('mouseout', () => {el.classList.remove('fa-shake')});
@@ -27,7 +36,6 @@ edit_name_button_real.addEventListener('click', () => {
 const save = document.getElementById('save-icon');
 save.addEventListener('click', async () => { 
     save.classList.add('fa-bounce');
-    console.log('HUH?!')
     await sleep(1000);
     save.classList.remove('fa-save');
     save.classList.add('fa-check'); 
@@ -37,4 +45,42 @@ save.addEventListener('click', async () => {
     save.classList.remove('fa-bounce'); 
     save.classList.add('fa-save');
     save.classList.remove('fa-beat-fade');
+});
+
+function updateLatLocUI() {   
+    [].slice.call(document.getElementsByClassName('curloc')).forEach(el => {
+        el.addEventListener('click', () => {
+            let par = el.parentElement;
+            ([].slice.call(par.getElementsByClassName('lat'))[0]).innerText = lat;
+            ([].slice.call(par.getElementsByClassName('lng'))[0]).innerText = lng;
+        });
+    });
+}
+
+updateLatLocUI();
+
+document.getElementById('add-loc').addEventListener('click', () => {
+    document.querySelector('placeholder').outerHTML = `
+<div class="subsection"><i class="fa-solid fa-location-dot"></i> Location
+    <div class="subsubsection"><i class="fa-solid fa-circle-arrow-up"></i> Lat<div class="subsubsubsection lat" contenteditable></div></div>
+    <div class="subsubsection"><i class="fa-solid fa-circle-arrow-left"></i> Lng<div class="subsubsubsection lng" contenteditable></div></div>
+    
+    <div class="subsubsection"><i class="fa-solid fa-table-cells"></i> Grid Position
+        <div class="grid">
+            <div class="subsubsubsection"><input type="checkbox"></div>
+            <div class="subsubsubsection"><input type="checkbox"></div>
+            <div class="subsubsubsection"><input type="checkbox"></div>
+            <div class="subsubsubsection"><input type="checkbox"></div>
+            <div class="subsubsubsection"><input type="checkbox"></div>
+            <div class="subsubsubsection"><input type="checkbox"></div>
+            <div class="subsubsubsection"><input type="checkbox"></div>
+            <div class="subsubsubsection"><input type="checkbox"></div>
+            <div class="subsubsubsection"><input type="checkbox"></div>
+            </div>
+    </div>
+    <div class="subsubsubsection curloc">Use Current Location</div>
+</div>
+<placeholder></placeholder>
+`;
+    updateLatLocUI();
 });
