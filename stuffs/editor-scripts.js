8ed9509d34b0021b57d319105d0a2749717269c2
PremPremPremPrem <prem.sy1952@gmail.com>
2023-09-23 11:03:33 +0700

    Lorem Ipsum
    lorem ipsum dolar sit amet

--- stuffs/editor-scripts.js
+++ stuffs/editor-scripts.js
@@ -15,9 +15,26 @@ function popup_thing(button, dialog, div) {
     div.addEventListener('click', (e) => e.stopPropagation());
 }
 
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
 shake_anim(edit_name_button);
 popup_thing(edit_name_button, edit_name_dialog, edit_name_div);
 edit_name_button_real.addEventListener('click', () => {
     edit_name_dialog.close();
     project_name.innerText = document.querySelector('span[contenteditable]').innerText;
+});
+
+const save = document.getElementById('save-icon');
+save.addEventListener('click', async () => { 
+    save.classList.add('fa-bounce');
+    console.log('HUH?!')
+    await sleep(1000);
+    save.classList.remove('fa-save');
+    save.classList.add('fa-check'); 
+    save.classList.add('fa-beat-fade');
+    await sleep(2000);
+    save.classList.remove('fa-check');
+    save.classList.remove('fa-bounce'); 
+    save.classList.add('fa-save');
+    save.classList.remove('fa-beat-fade');
 });
