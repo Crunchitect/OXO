c6e0cf0fc2d09f1600b71e02236c95b89cec3b62
Phatphum Tarsaku <81572783+phatphumt@users.noreply.github.com>
2023-10-01 05:30:16 +0000

    some refactoring

--- stuffs/sign-in.js
+++ stuffs/sign-in.js
@@ -6,15 +6,19 @@ document.querySelector('#submit').addEventListener('click', async () => {
     const hash = sha256(username + password);
     var account = '';
     try {
-        account = JSON.parse(
-            await (await fetch(`https://tictactie.pythonanywhere.com/signin/${hash}`)).text()
-        );
-    } catch (err) {
-        caution.innerText = 'There\'s a problem on our end, please try again.';
+        const stuff = await fetch(`https://tictactie.pythonanywhere.com/signin/${hash}`)
+        const text = await stuff.text()
+        account = JSON.parse(text)
+    } catch (e) {
+        caution.innerText = `There's a problem, please try again (${e})`
         return;
     }
 
-    if (account == -1) {caution.innerText = 'Username or Password is not coorect.'; return;}
+    if (account == -1) {
+        caution.innerText = 'Username or Password is not coorect.'; 
+        return;
+    }
+    
     localStorage.setItem('oxo-username', account.username);
     localStorage.setItem('oxo-token', account.token);
     location.replace('index.html');
